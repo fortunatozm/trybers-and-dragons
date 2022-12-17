bea001861b6c8d5226eb9a951d2fa2fa5a96167b
fortunatozm <fortunatompongo@hotmail.com>
2022-12-17 20:53:36 -0300

    10 - Crie a classe PVP - OK

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -1,5 +1,25 @@
+import Fighter from '../Fighter';
 import Battle from './Battle';
 
 export default class PVP extends Battle {
-  
+  private _player2 : Fighter;
+
+  constructor(protected player : Fighter, player2 : Fighter) {
+    super(player);
+    this._player2 = player2;
+  }
+
+  fight(): number {
+    for (let index = 0; index < 100; index += 1) {
+      this.player.attack(this._player2);
+      if (this._player2.lifePoints <= 0) {
+        return 1;
+      }
+      this._player2.attack(this.player);
+      if (this.player.lifePoints <= 0) {
+        return -1;
+      }
+    }
+    return 0;
+  }
 }
