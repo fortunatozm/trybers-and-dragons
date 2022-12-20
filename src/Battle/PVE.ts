75a2a2ffdb60119e32cb3200b9a2fc7e46f1f262
fortunatozm <fortunatompongo@hotmail.com>
2022-12-19 21:56:22 -0300

    11 - Criar a classe PVE - OK

--- src/Battle/PVE.ts
+++ src/Battle/PVE.ts
@@ -1,5 +1,28 @@
+import Fighter, { SimpleFighter } from '../Fighter';
 import Battle from './Battle';
 
 export default class PVE extends Battle {
+  // private _beings: SimpleFighter[];
+  constructor(player: Fighter, private beings: SimpleFighter[]) {
+    super(player);
+    // this._beings = beings;
+  }
 
+  fighters(i: number): number {
+    for (let index = 0; index < 100; index += 1) {
+      this.player.attack(this.beings[i]);
+      if (this.beings[i].lifePoints <= 0) return 1;
+      this.beings[i].attack(this.player);
+      if (this.player.lifePoints <= 0) return -1;
+    }
+    return 0;
+  }
+
+  fight(): number {
+    for (let index = 0; index < this.beings.length; index += 1) {
+      const shift = this.fighters(index);
+      if (shift === -1) return -1;
+    }
+    return 1;
+  }
 }
